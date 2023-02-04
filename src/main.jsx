3ce103ae6f5bcc3f75bf8f579fa2975f2f7c775b
Dam450 <68165752+dam450@users.noreply.github.com>
2023-02-04 14:53:47 -0300

    create page Preview

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,13 @@ import { ThemeProvider } from 'styled-components'
 import { GlobalStyle } from '@/styles/global'
 import theme from '@/styles/theme'
 
-import App from './App.jsx'
+import { Preview } from '@/pages/Preview'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <App />
+      <Preview />
     </ThemeProvider>
   </React.StrictMode>
 )
---
+++ src/pages/Preview/index.jsx
@@ -0,0 +1,10 @@
+import { Header } from '@/components/Header';
+import { Container } from './styles.js';
+
+export function Preview() {
+  return (
+    <Container>
+      <Header />
+    </Container>
+  );
+}
