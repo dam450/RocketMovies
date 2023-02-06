2e0f111552aeaa361a0a8cdf860b5ffbcd113279
Dam450 <68165752+dam450@users.noreply.github.com>
2023-02-06 16:36:06 -0300

    update page Preview

--- src/pages/Preview/styles.js
+++ src/pages/Preview/styles.js
@@ -1,6 +1,10 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+  * {
+    /* border: 1px solid green; */
+  }
+
   width: 100%;
   height: 100vh;
 
@@ -10,4 +14,73 @@ export const Container = styled.div`
   grid-template-areas:
     'header'
     'content';
+`;
+
+export const Content = styled.main`
+  grid-area: content;
+
+  margin: 4rem 12rem 15rem;
+  padding-right: 2rem;
+
+  height: 70vh;
+  overflow-y: auto;
+
+  > a:first-child {
+    color: ${({theme}) => theme.COLORS.RED_100}; 
+    display: inline-flex;
+    align-items: center;
+    
+
+    margin-bottom: 2.4rem;
+  }
+
+  > p {
+    font-weight: 400;
+    font-size: 1.6rem;
+    line-height: 130%;
+
+    display: flex;
+    align-items: center;
+    text-align: justify;
+  }
+`;
+
+export const Title = styled.h2`
+  font-weight: 500;
+  font-size: 36px;
+  line-height: 47px;
+  margin-bottom: 2.4rem;
+`;
+
+export const Author = styled.cite`
+  font-family: 'Roboto';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 1.6rem;
+  line-height: 1.9rem;
+
+  margin-bottom: 4rem;
+
+  display: flex;
+  align-items: center;
+  gap: 0.8rem;
+
+  > span {
+    margin-right: 0.8rem;
+    display: flex;
+    align-items: center;
+
+    > img, svg {
+      transform: translateY(-5%);
+      margin-right: 5px;
+    }
+
+    svg {
+      stroke: ${({ theme }) => theme.COLORS.RED_100};
+    }
+  }
+`;
+
+export const Tags = styled.div`
+  margin-bottom: 4rem;
 `;
