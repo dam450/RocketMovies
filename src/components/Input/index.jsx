e5f1f097e3ac5e05ebf06581e70f7dc9fd481be0
Dam450 <68165752+dam450@users.noreply.github.com>
2023-02-14 00:03:32 -0300

    update component Input

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -3,6 +3,7 @@ import { Container } from './styles';
 export function Input({ icon: Icon, ...rest }) {
   return (
     <Container>
+      <label htmlFor=""></label>
       {Icon && <Icon size={20} />}
       <input {...rest} />
     </Container>
--- src/components/Input/styles.js
+++ src/components/Input/styles.js
@@ -4,7 +4,7 @@ export const Container = styled.div`
   background-color: ${({ theme }) => theme.COLORS.BACKGROUND_700};
   border-radius: 10px;
 
-  padding: 1.6rem;
+  padding: 0 1.6rem;
 
   width: min(100%, 50rem);
   min-width: 20rem;
@@ -22,13 +22,15 @@ export const Container = styled.div`
     border: none;
     background-color: transparent;
 
+    padding: 1.6rem 0;
     width: 100%;
 
     color: ${({ theme }) => theme.COLORS.WHITE_100};
     font-size: 1.4rem;
     line-height: 1.8rem;
   }
+
   > svg {
-    color: ${({ theme }) => theme.COLORS.GRAY_300}; 
+    color: ${({ theme }) => theme.COLORS.GRAY_300};
   }
 `;
