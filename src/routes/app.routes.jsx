ccd503322b030a272d12112fb83d16531acc48e4
Dam450 <68165752+dam450@users.noreply.github.com>
2023-02-14 00:07:48 -0300

    add Profile route

--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -1,13 +1,15 @@
 import { Routes, Route } from 'react-router-dom'
 
-import { Preview } from '@/pages/Preview'
 import { Home } from '@/pages/Home'
+import { Preview } from '@/pages/Preview'
+import { Profile } from '@/pages/Profile'
 
 export function AppRoutes() {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/preview' element={<Preview />} />
+      <Route path='/profile' element={<Profile />} />
     </Routes>
   )
 }
