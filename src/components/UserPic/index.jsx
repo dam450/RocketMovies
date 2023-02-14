30617eea1554b92c95c3e5fbd4b50cfe1ced700e
Dam450 <68165752+dam450@users.noreply.github.com>
2023-02-14 00:02:24 -0300

    update component UserPic

--- src/components/UserPic/index.jsx
+++ src/components/UserPic/index.jsx
@@ -1,7 +1,7 @@
 import { Container } from './styles.js';
 
-export function UserPic({size, ...rest}) {
+export function UserPic({size, src, ...rest}) {
   return (
-      <Container size={size} {...rest} />
+      <Container size={size} src={src} {...rest} />
   );
 }
