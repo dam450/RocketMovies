83daa022d58d061b3e84f7ca995edf5537b9a997
Dam450 <68165752+dam450@users.noreply.github.com>
2023-02-14 11:30:20 -0300

    add route create

--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -5,6 +5,7 @@ import { Preview } from '@/pages/Preview'
 import { Profile } from '@/pages/Profile'
 import { SignIn } from '@/pages/SignIn'
 import { SignUp } from '@/pages/SignUp'
+import { NewMovie } from '@/pages/NewMovie'
 
 export function AppRoutes() {
   return (
@@ -14,6 +15,7 @@ export function AppRoutes() {
       <Route path='/profile' element={<Profile />} />
       <Route path='/signin' element={<SignIn />} />
       <Route path='/signup' element={<SignUp />} />
+      <Route path='/create' element={<NewMovie />} />
     </Routes>
   )
 }
