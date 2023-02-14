1c0da76022120ea30ab3527c03804010fc16cf75
Dam450 <68165752+dam450@users.noreply.github.com>
2023-02-14 16:39:12 -0300

    update page NewMovie

--- src/pages/NewMovie/index.jsx
+++ src/pages/NewMovie/index.jsx
@@ -1,13 +1,55 @@
-import { Container } from './styles'
+import { Link } from 'react-router-dom'
+import { FiArrowLeft } from 'react-icons/fi'
 
-//TODO: complete newMovie page
-// ? need new components
+import { Button } from '@/components/Button'
+import { Header } from '@/components/Header'
+import { Input } from '@/components/Input'
+import { TextArea } from '@/components/TextArea'
+
+import { Container, Content } from './styles'
+import { NewTag } from '@/components/NewTag'
 
 export function NewMovie() {
   return (
     <Container>
-      <h2>new movie</h2>
+      <Header />
+      <Content>
+        <Link to="/">
+          <FiArrowLeft size={16} />
+          Voltar
+        </Link>
 
+        <h2>Novo filme</h2>
+        <form action="">
+          <div className="row">
+            <Input placeholder="Título"></Input>
+            <Input placeholder="Sua nota (de 0 a 5)"></Input>
+          </div>
+          <TextArea placeholder="Observações" />
+
+          <h3>Marcadores</h3>
+          <div className="markers">
+
+            <NewTag
+              placeholder=""
+              value="clássico clássico clássico clássico"
+            />
+            <NewTag
+              placeholder=""
+              value="clássico"
+            />
+            <NewTag
+              isNew
+              placeholder="Novo marcador"
+            />
+          </div>
+
+          <div className="row">
+            <Button title="Excluir filme" type="button" />
+            <Button title="Salvar alterações" type="button" />
+          </div>
+        </form>
+      </Content>
     </Container>
   )
 }
--- src/pages/NewMovie/styles.js
+++ src/pages/NewMovie/styles.js
@@ -1,7 +1,84 @@
 import styled from 'styled-components'
 
-
-//TODO: format newMovie page
-
 export const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+
+  display: grid;
+  grid-template-rows: 11.6rem auto;
+
+  grid-template-areas:
+    'header'
+    'content';
+`;
+
+export const Content = styled.div`
+  grid-area: content;
+
+  margin: 4rem 12rem 15rem;
+  padding: 0 2rem 0 0.4rem;
+
+  height: 70vh;
+  overflow-y: scroll;
+
+  > a:first-child {
+    color: ${({ theme }) => theme.COLORS.RED_100};
+    display: inline-flex;
+    align-items: center;
+
+    margin: 3px 3px 2.4rem;
+  }
+
+  h2 {
+    font-size: 3.6rem;
+    line-height: 4.7rem;
+    color: ${({ theme }) => theme.COLORS.WHITE_200};
+    margin-bottom: 4rem;
+  }
+
+  h3 {
+    font-weight: 400;
+    font-size: 2rem;
+    line-height: 2.6rem;
+    color: ${({ theme }) => theme.COLORS.GRAY_400};
+    margin: 4rem 0 2.4rem;
+  }
+
+  .markers {
+    display: flex;
+    width: 100%;
+    gap: 2rem;
+    border-radius: 8px;
+
+    margin-bottom: 4rem;
+
+    background-color: ${({ theme }) => theme.COLORS.BACKGROUND_900};
+    min-height: 8rem;
+
+    padding: 1.6rem;
+
+    flex-wrap: wrap;
+  }
+
+  .row {
+    display: inline-flex;
+    width: 100%;
+    gap: 4rem;
+    border-radius: 8px;
+
+    margin-bottom: 4rem;
+
+    div {
+      width: 100%;
+    }
+
+    button {
+      width: 100%;
+
+      &:first-child {
+        background-color: ${({ theme }) => theme.COLORS.BACKGROUND_900};
+        color: ${({ theme }) => theme.COLORS.RED_100};
+      }
+    }
+  }
 `;
