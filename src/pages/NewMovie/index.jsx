0e875dc142dc7720da299ded1a3fb6c723f9c9b2
Dam450 <68165752+dam450@users.noreply.github.com>
2023-02-14 11:29:55 -0300

    create page NewMovie

---
+++ src/pages/NewMovie/index.jsx
@@ -0,0 +1,13 @@
+import { Container } from './styles'
+
+//TODO: complete newMovie page
+// ? need new components
+
+export function NewMovie() {
+  return (
+    <Container>
+      <h2>new movie</h2>
+
+    </Container>
+  )
+}
---
+++ src/pages/NewMovie/styles.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+
+//TODO: format newMovie page
+
+export const Container = styled.div`
+`;
