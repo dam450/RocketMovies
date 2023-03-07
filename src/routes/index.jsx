5148c7d56d3e9558f742baa87aaf9492cda2c4b2
Dam450 <68165752+dam450@users.noreply.github.com>
2023-03-07 16:24:07 -0300

    separate auth routes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,12 +1,13 @@
 import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./app.routes";
+import { AuthRoutes } from "./auth.routes";
 
+const authenticated = false
 
 export function Routes() {
   return (
     <BrowserRouter>
-      <AppRoutes />
+      {authenticated ? <AppRoutes /> : <AuthRoutes />}
     </BrowserRouter>
   )
 }
-  
