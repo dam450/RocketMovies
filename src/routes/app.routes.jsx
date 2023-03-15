540d8517fdcce80777fc4d468cc77922e50c1692
Dam450 <68165752+dam450@users.noreply.github.com>
2023-03-15 15:16:58 -0300

    update routes

--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -1,21 +1,21 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 
 import { Home } from '@/pages/Home'
 import { Preview } from '@/pages/Preview'
 import { Profile } from '@/pages/Profile'
-import { SignIn } from '@/pages/SignIn'
-import { SignUp } from '@/pages/SignUp'
 import { NewMovie } from '@/pages/NewMovie'
 
 export function AppRoutes() {
   return (
     <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/preview' element={<Preview />} />
-      <Route path='/profile' element={<Profile />} />
-      <Route path='/signin' element={<SignIn />} />
-      <Route path='/signup' element={<SignUp />} />
-      <Route path='/create' element={<NewMovie />} />
+      <Route path="/" element={<Home />} />
+      <Route path="/preview" element={<Preview />} />
+      <Route path="/preview/:id" element={<Preview />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/create" element={<NewMovie />} />
+      <Route path="/change/:id" element={<NewMovie />} />
+
+      {/* <Route path="*" element={<Navigate to={'/'} replace />} /> */}
     </Routes>
   )
 }
