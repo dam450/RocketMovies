d912b45ec165030c649a8a0e592ef10adc42db06
Dam450 <68165752+dam450@users.noreply.github.com>
2023-03-15 15:34:37 -0300

    doc: util getAvatarURL

--- src/utils/getAvatarURL.js
+++ src/utils/getAvatarURL.js
@@ -4,6 +4,11 @@ import AvatarPlaceholder from '@/assets/avatar_placeholder.svg'
 const baseURL = api.defaults.baseURL
 const resource = 'files'
 
+/**
+ * Retorna a URL da imagem do usuário. Se o nome for nulo, retorna a URL da imagem padrão.
+ * @param {String} avatar nome da imagem no banco de dados
+ * @returns {URL} URL da imagem
+ */
 export function getAvatarURL(avatar) {
 
   if (!avatar) return AvatarPlaceholder
