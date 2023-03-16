dd9c2f359265e50929164205811f83c8c51b120b
Dam450 <68165752+dam450@users.noreply.github.com>
2023-03-16 15:04:58 -0300

    fetch data to preview

--- src/pages/Preview/index.jsx
+++ src/pages/Preview/index.jsx
@@ -1,15 +1,63 @@
+import { useEffect, useState } from 'react'
+import { Link, useParams } from 'react-router-dom';
+
 import { Header } from '@/components/Header';
 import { Stars } from '@/components/Stars';
 import { Tag } from '@/components/Tag';
 import { UserPic } from '@/components/UserPic';
 
-
-
-import { FiArrowLeft, FiClock } from 'react-icons/fi';
-import { Link } from 'react-router-dom';
+import { FiArrowLeft, FiClock, FiEdit } from 'react-icons/fi';
 import { Container, Content, Title, Author, Tags } from './styles';
 
+import { api } from '@/services/api';
+import { getAvatarURL } from '@/utils/getAvatarURL';
+import { useAuth } from '@/hooks/auth';
+import { ButtonLink, Button } from '@/components/Button';
+
 export function Preview() {
+  const [ movieData, setMovieData ] = useState({})
+  const [ date, setDate ] = useState('')
+  const [ time, setTime ] = useState('')
+  const { user } = useAuth()
+
+  const avatarURL = getAvatarURL(user.avatar)
+
+  const { id: movieId } = useParams()
+  // console.log('param:', movieId)
+
+  function fetchMovieData(id) {
+    api.get(`/movies/${id}`)
+      .then(response => response.data)
+      .then(movie => {
+        setMovieData(movie)
+        // console.log('movieData:', movie)
+      }).catch(err => console.error(err))
+  }
+
+
+  useEffect(() => {
+    fetchMovieData(movieId)
+  }, [])
+
+
+  useEffect(() => {
+    if (movieData.created_at) {
+      const movieDate = new Date(`${movieData.created_at} UTC`)
+
+      const dateOptions = {
+        dateStyle: 'short',
+        timeStyle: 'short',
+      }
+      const datetime = new Intl.DateTimeFormat('pt-BR', dateOptions)
+        .format(movieDate)
+        .split(',')
+
+      setDate(datetime[0])
+      setTime(datetime[1])
+    }
+  }, [movieData])
+
+
   return (
     <Container>
       <Header />
@@ -19,46 +67,29 @@ export function Preview() {
           Voltar
         </Link>
         <Title>
-          Interestellar <Stars onChange={(rate) => alert(rate)} readonly initialRating={4} />
+          {movieData.title}{' '}
+          <Stars
+            onChange={rate => alert(rate)}
+            readonly
+            initialRating={movieData.rating}
+          />
         </Title>
         <Author>
           <span>
-            <UserPic size={16} src='https://github.com/dam450.png' alt='imagem do usuário' />
-            Por Evandro Damaso
+            <UserPic size={16} src={avatarURL} alt={user.name} />
+            Por {user.name}
           </span>
           <span>
-            <FiClock size={16} /> 23/05/22 às 08:00
+            <FiClock size={16} /> {date} às {time}
           </span>
         </Author>
         <Tags>
-          <Tag value='Ficção' />
-          <Tag value={'Científica'} />
-          <Tag value={'Drama'} />
-          <Tag value={'Família'} />
+          {movieData.tags &&
+            movieData.tags.map(tag => <Tag key={tag} value={tag} />)}
         </Tags>
-        <p>
-          Pragas nas colheitas fizeram a civilização humana regredir para uma sociedade agrária em futuro de data
-          desconhecida. Cooper, ex-piloto da NASA, tem uma fazenda com sua família. Murphy, a filha de dez anos de
-          Cooper, acredita que seu quarto está assombrado por um fantasma que tenta se comunicar com ela. Pai e filha
-          descobrem que o "fantasma" é uma inteligência desconhecida que está enviando mensagens codificadas através de
-          radiação gravitacional, deixando coordenadas em binário que os levam até uma instalação secreta da NASA
-          liderada pelo professor John Brand. O cientista revela que um buraco de minhoca foi aberto perto de Saturno e
-          que ele leva a planetas que podem oferecer condições de sobrevivência para a espécie humana. As "missões
-          Lázaro" enviadas anos antes identificaram três planetas potencialmente habitáveis orbitando o buraco negro
-          Gargântua: Miller, Edmunds e Mann – nomeados em homenagem aos astronautas que os pesquisaram. Brand recruta
-          Cooper para pilotar a nave espacial Endurance e recuperar os dados dos astronautas; se um dos planetas se
-          mostrar habitável, a humanidade irá seguir para ele na instalação da NASA, que é na realidade uma enorme
-          estação espacial. A partida de Cooper devasta Murphy. <br />
-          <br />
-          Além de Cooper, a tripulação da Endurance é formada pela bióloga Amelia, filha de Brand; o cientista Romilly,
-          o físico planetário Doyle, além dos robôs TARS e CASE. Eles entram no buraco de minhoca e se dirigem a Miller,
-          porém descobrem que o planeta possui enorme dilatação gravitacional temporal por estar tão perto de Gargântua:
-          cada hora na superfície equivale a sete anos na Terra. Eles entram em Miller e descobrem que é inóspito já que
-          é coberto por um oceano raso e agitado por ondas enormes. Uma onda atinge a tripulação enquanto Amelia tenta
-          recuperar os dados de Miller, matando Doyle e atrasando a partida. Ao voltarem para a Endurance, Cooper e
-          Amelia descobrem que 23 anos se passaram.
-        </p>
+        <p>{movieData.description}</p>
+        <Link to={`/change/${movieData.id}`}>Editar filme</Link>
       </Content>
     </Container>
-  );
+  )
 }
