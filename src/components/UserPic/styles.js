bd43aa8a300106df77a76eabf1526549aa5d562d
Dam450 <68165752+dam450@users.noreply.github.com>
2023-03-16 15:08:28 -0300

    avatar aspect fix

--- src/components/UserPic/styles.js
+++ src/components/UserPic/styles.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 export const Container = styled.img`
   width: ${({ size }) => (size ? `${size}px` : 'auto')};
   height: ${({ size }) => (size ? `${size}px` : 'auto')};
+  aspect-ratio: 1 / 1;
 
   border-radius: 50%;
   border: 1px solid ${({ theme }) => theme.COLORS.GRAY_700};
