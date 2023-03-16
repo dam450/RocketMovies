d4201de11e29932389dee5fbfd359c8b33bf30eb
Dam450 <68165752+dam450@users.noreply.github.com>
2023-03-16 15:07:29 -0300

    handle profile and avatar update

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,12 +1,56 @@
-import { Link } from 'react-router-dom';
-import { FiArrowLeft, FiCamera, FiLock, FiMail, FiUser } from 'react-icons/fi';
+import { useAuth } from '@/hooks/auth'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { FiArrowLeft, FiCamera, FiLock, FiMail, FiUser } from 'react-icons/fi'
 
-import { Container, Header, Content, Avatar } from './styles';
-import { UserPic } from '@/components/UserPic';
-import { Input } from '@/components/Input';
-import { Button } from '@/components/Button';
+import { getAvatarURL } from '@/utils/getAvatarURL'
+
+import { Container, Header, Content, Avatar } from './styles'
+import { UserPic } from '@/components/UserPic'
+import { Input } from '@/components/Input'
+import { Button } from '@/components/Button'
 
 export function Profile() {
+
+  const { user, updateUser } = useAuth()
+  const [name, setName] = useState(user.name)
+  const [email, setEmail] = useState(user.email)
+  const [currentPassword, setCurrentPassword] = useState('')
+  const [newPassword, setNewPassword] = useState('')
+
+  const avatarURL = getAvatarURL(user.avatar)
+  const [avatar, setAvatar] = useState(avatarURL)
+  const [avatarFile, setAvatarFile] = useState(null)
+
+  async function handleUpdateProfile(e) {
+    e.preventDefault()
+
+    const user = {
+      name,
+      email,
+      password: currentPassword,
+      new_password: newPassword
+    }
+
+    await updateUser({ user, avatarFile })
+
+    setCurrentPassword('')
+    setNewPassword('')
+  }
+
+  function handleImageKeydown(e) {
+    if(e.code === 'Space' || e.code === 'Enter')
+      e.target.click()
+  }
+
+  function handleChangeAvatar(event) {
+    const file = event.target.files[ 0 ]
+    setAvatarFile(file)
+
+    const imagePreview = URL.createObjectURL(file)
+    setAvatar(imagePreview)
+  }
+
   return (
     <Container>
       <Header>
@@ -21,16 +65,21 @@ export function Profile() {
           <Avatar>
             <UserPic
               size={186}
-              src="https://github.com/dam450.png"
+              src={avatar ? avatar : avatarURL}
               alt="avatar do usuário"
             />
-            <label htmlFor="avatar" tabIndex="0">
+            <label
+              htmlFor="avatar"
+              tabIndex="0"
+              onKeyDown={e => handleImageKeydown(e)}
+            >
               <FiCamera />
               <input
                 type="file"
                 id="avatar"
                 accept="image/png, image/jpeg"
                 className="hidden"
+                onChange={handleChangeAvatar}
               />
             </label>
           </Avatar>
@@ -40,27 +89,35 @@ export function Profile() {
             placeholder="Nome do usuário"
             type="text"
             autoComplete="off"
+            value={name}
+            onChange={e => setName(e.target.value)}
           />
           <Input
             icon={FiMail}
             placeholder="E-mail"
             type="email"
             autoComplete="off"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
           />
           <Input
             icon={FiLock}
             placeholder="Senha atual"
             type="password"
             autoComplete="off"
+            value={currentPassword}
+            onChange={e => setCurrentPassword(e.target.value)}
           />
           <Input
             icon={FiLock}
             placeholder="Nova senha"
             type="password"
             autoComplete="off"
+            value={newPassword}
+            onChange={e => setNewPassword(e.target.value)}
           />
 
-          <Button title="Salvar" type="button" />
+          <Button title="Salvar" type="submit" onClick={handleUpdateProfile} />
         </form>
       </Content>
     </Container>
